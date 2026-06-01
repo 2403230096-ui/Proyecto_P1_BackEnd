bb6690b19ac2f1c381dd2c32c7b671207dc99e7b
MAL <2403230096@cesun.edu.mx>
2026-05-31 21:50:08 -0700

    Finalizacion de app / adicion de coordenadas y color de relleno aleatorio

--- app.py
+++ app.py
@@ -1,26 +1,60 @@
 from p5 import * 
 from classes.clases import *
 from patron.Builders import Builder
+import random
 
 
 cuadrado = None
+cuadrado2 = None
 elipse = None
+elipse2 = None
 # Figuras
 ventana = Ventana(window_width=800, window_height=800)
 figuras = []
 
+def colorAleatorio():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return f"#{r:02X}{g:02X}{b:02X}"
+
 def setup():
+
     ventana.iniciar()
     global cuadrado
+    global cuadrado2
     global elipse
+    global elipse2
 
-    cuadrado = Cuadrado(2, "#0045A0", 50, 50, 10, 0, 3, 2, "#862F2F")
-    elipse = Elipse(2, "#7C3E3E", 50, 50, 100, 100, 3, 2, "#CE22D4")
+
+    cuadrado = Cuadrado(2, "#0045A0", 50, 50,
+                        random.uniform(0, ventana.window_width  - 50),   # x aleatorio
+                        random.uniform(0, ventana.window_height - 50),   # y aleatorio
+                        3, 2,
+                        colorAleatorio())                                 # color aleatorio
+    
+    cuadrado2 = Cuadrado(2, "#0045A0", 50, 50,
+                        random.uniform(0, ventana.window_width  - 50),   # x aleatorio
+                        random.uniform(0, ventana.window_height - 50),   # y aleatorio
+                        3, 2,
+                        colorAleatorio())   
+
+    elipse = Elipse(2, "#7C3E3E", 50, 50,
+                    random.uniform(0, ventana.window_width  - 50),       # x aleatorio
+                    random.uniform(0, ventana.window_height - 50),       # y aleatorio
+                    3, 2,
+                    colorAleatorio())   
+    
+    elipse2 = Elipse(2, "#7C3E3E", 50, 50,
+                    random.uniform(0, ventana.window_width  - 50),       # x aleatorio
+                    random.uniform(0, ventana.window_height - 50),       # y aleatorio
+                    5, 2,
+                    colorAleatorio())  
 
     figuras.append(cuadrado)
+    figuras.append(cuadrado2)
     figuras.append(elipse)
-
-
+    figuras.append(elipse2)
 
 def draw():
     background(220)
